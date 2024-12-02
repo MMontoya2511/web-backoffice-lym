029e67a58d03e68864c104420d62a951bd6e8464
mmontoya <marcelomontoyaflores2511@gmail.com>
2024-12-02 10:49:44 -0500

    Agregando mis fuentes iniciales

---
+++ src/main/java/pe/edu/cibertec/web_backoffice_lym/dto/FilmDto.java
@@ -0,0 +1,8 @@
+package pe.edu.cibertec.web_backoffice_lym.dto;
+
+public record FilmDto(Integer filmId, String title, String language, Integer rentalDuration, Double rentalRate ) {
+
+
+
+
+}
---
+++ src/main/java/pe/edu/cibertec/web_backoffice_lym/service/MaintenanceService.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.web_backoffice_lym.service;
+
+import pe.edu.cibertec.web_backoffice_lym.dto.FilmDto;
+
+import java.util.List;
+
+public interface MaintenanceService {
+
+    List<FilmDto> findAllFilms();
+
+
+
+
+}
